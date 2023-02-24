ba1783a972b3c0166ae187869db4e699d12029e7
kiboma <omayioben@gmail.com>
2023-02-24 08:34:35 +0300

    Add lorem with 150words

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
     <h1>This is Job hunter Home Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, hic! Tenetur dolores maxime nisi eius doloribus 
+      tempora neque, deleniti placeat expedita aliquid nam aperiam repellat sit quisquam fuga perspiciatis, odit, possimus dolore in. 
+      Maiores molestias molestiae magnam eaque explicabo quidem necessitatibus vitae consequuntur nobis quisquam aspernatur 
+      voluptatibus nemo alias nostrum ex ipsa, qui eum optio reprehenderit sed quibusdam magni! </p>
+      
+      <p>Natus deleniti veniam praesentium nam aliquam maxime, magni iure provident voluptas commodi facere eum 
+      labore necessitatibus dolorum soluta. 
+      Nobis repellendus maiores at odit sequi, eligendi ipsa incidunt quia officiis modi quaerat aliquid quasi recusandae dolorum et aliquam, 
+      exercitationem doloribus mollitia veniam quidem perferendis. Quibusdam aliquid inventore eum soluta temporibus dolor earum nostrum nihil cum. </p>
+      
+      <p>Nobis dolorem sapiente aperiam corrupti expedita fugit porro sint, distinctio placeat asperiores non amet atque voluptates! Provident, 
+      assumenda! Eum assumenda nisi quidem voluptatibus autem recusandae odio, illo, quasi eligendi neque tenetur officiis dolores. 
+      Assumenda quasi beatae nostrum?` `</p>
   </div>
 </template>
 
